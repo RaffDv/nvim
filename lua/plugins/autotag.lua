61c4376960e65bf6a7a75470ccf4840239e5c573
Rafael Vargas <rafael.vargas.emp@gmail.com>
2025-07-14 16:52:59 -0300

    fix: some errors

--- lua/plugins/autotag.lua
+++
@@ -1,5 +0,0 @@
-return {
-  "tronikelis/ts-autotag.nvim",
-  ft = { "html", "javascriptreact", "typescriptreact" },
-  opts = {},
-}
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -22,4 +22,46 @@ return {
   --add metal-black
   --
   { "metalelf0/base16-black-metal-scheme" },
+
+  -- add vesper
+  { "datsfilipe/vesper.nvim" },
+
+  -- add zenbones
+  {
+    "zenbones-theme/zenbones.nvim",
+    dependencies = "rktjmp/lush.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+
+  -- add vimdark
+  { "ldelossa/vimdark" },
+
+  -- add rosepine
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+  },
+
+  -- add github theme
+
+  {
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("github-theme").setup({
+        options = {
+          styles = {
+            comments = "italic",
+            keywords = "bold",
+            types = "italic,bold",
+          },
+        },
+      })
+
+      vim.cmd("colorscheme github_dark_high_contrast")
+    end,
+  },
 }
