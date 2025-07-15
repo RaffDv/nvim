af2b1fdef16c35ae2d0b4af22cb32b2b0e0bd017
Rafael Mattos de Vargas <113399703+RaffDv@users.noreply.github.com>
2025-07-15 01:30:49 -0300

    Update prettier.lua

--- lua/plugins/prettier.lua
+++ lua/plugins/prettier.lua
@@ -3,10 +3,6 @@ return {
     "stevearc/conform.nvim",
     opts = function(_, opts)
       opts.formatters_by_ft = {
-        ["javascript"] = { "prettier" },
-        ["typescript"] = { "prettier" },
-        ["javascriptreact"] = { "prettier" },
-        ["typescriptreact"] = { "prettier" },
         ["css"] = { "prettier" },
         ["html"] = { "prettier" },
         ["json"] = { "prettier" },
